1880dea9234f9daa4102a7cbad65c0f285a9f74f
Yathartha Goel <ygoel@asu.edu>
2018-04-23 21:56:12 -0700

    Added structure for body vitals in client

---
+++ src/main/java/ser516/project3/client/Components/BodyVitals/BodyVitalsModel.java
@@ -0,0 +1,10 @@
+package ser516.project3.client.Components.BodyVitals;
+
+import ser516.project3.constants.ClientConstants;
+import ser516.project3.interfaces.ModelInterface;
+
+import java.awt.*;
+
+public class BodyVitalsModel implements ModelInterface {
+
+}
---
+++ src/main/java/ser516/project3/client/Components/BodyVitals/BodyVitalsView.java
@@ -0,0 +1,26 @@
+package ser516.project3.client.Components.BodyVitals;
+
+import java.util.EventListener;
+
+import ser516.project3.interfaces.ModelInterface;
+
+public class BodyVitalsView extends BodyVitalsAbstractView {
+
+	public BodyVitalsView(BodyVitalsModel bodyVitalsModel) {
+		super(bodyVitalsModel);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void updateView(ModelInterface model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addListener(EventListener eventListener, String listenerType, String componentName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
