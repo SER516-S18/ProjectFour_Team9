b79f20afa24abb73d30765fcfce7565b46e870cb
Yathartha Goel <ygoel@asu.edu>
2018-04-23 21:56:12 -0700

    Added structure for body vitals in client

---
+++ src/main/java/ser516/project3/client/Components/BodyVitals/BodyVitalsDataObservable.java
@@ -0,0 +1,11 @@
+package ser516.project3.client.Components.BodyVitals;
+
+import ser516.project3.client.Components.Graph.CoordinatesModel;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+
+public class BodyVitalsDataObservable extends Observable {
+
+}
