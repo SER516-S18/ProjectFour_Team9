6b3683158550baf4278a6b11c39e2ae837a5fbd3
ravi teja thoram <ravitejathoram@gmail.com>
2018-04-26 18:17:55 -0700

    Body Vitals Data Observable

--- src/main/java/ser516/project3/client/Components/BodyVitals/BodyVitalsDataObservable.java
+++ src/main/java/ser516/project3/client/Components/BodyVitals/BodyVitalsDataObservable.java
@@ -7,5 +7,33 @@ import java.util.Observable;
 
 
 public class BodyVitalsDataObservable extends Observable {
+	
+	private static BodyVitalsDataObservable instance;
+    private ArrayList<ArrayList<CoordinatesModel>> bodyVitalsData;
+	
+    private BodyVitalsDataObservable() {
+    	bodyVitalsData = new ArrayList<ArrayList<CoordinatesModel>>();
+    }
+    
+    public ArrayList<ArrayList<CoordinatesModel>> getBodyVitalsData() {
+        return bodyVitalsData;
+    }
+    
+    public static BodyVitalsDataObservable getInstance() {
 
+        try {
+            if (instance == null) {
+                instance = new BodyVitalsDataObservable();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return instance;
+    }
+    
+    public void addToListValues(ArrayList<CoordinatesModel> valueToBeAdded) {
+        this.bodyVitalsData.add(valueToBeAdded);
+        setChanged();
+        this.notifyObservers();
+    }
 }
