13a52cefa7c393342438145d32d04c9626249c10
Garv <gmathur1@asu.edu>
2018-04-26 17:31:32 -0700

    added health controller view and model

---
+++ src/main/java/ser516/project3/server/Components/Health/HealthAbstractView.java
@@ -0,0 +1,19 @@
+package ser516.project3.server.Components.Health;
+
+import ser516.project3.interfaces.ModelInterface;
+import ser516.project3.interfaces.ViewInterface;
+
+import javax.swing.*;
+
+public class HealthAbstractView extends JPanel implements ViewInterface {
+
+    @Override
+    public void initializeView(ViewInterface[] subViews) {
+
+    }
+
+    @Override
+    public void updateView(ModelInterface model) {
+
+    }
+}
---
+++ src/main/java/ser516/project3/server/Components/Health/HealthModel.java
@@ -0,0 +1,8 @@
+package ser516.project3.server.Components.Health;
+
+import ser516.project3.interfaces.ModelInterface;
+
+public class HealthModel implements ModelInterface {
+
+
+}
